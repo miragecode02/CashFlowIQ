7203534a1d060ccf7a77bd38ee68fd030f715e25
Mirage verma <miragecode02@gmail.com>
2026-07-04 18:28:12 +0530

    Update backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,13 +22,12 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:5173",
         "http://localhost:8080",
-        "http://localhost:8001",
+        "http://localhost:8081",
+        "http://localhost:5173",
         "http://127.0.0.1:8080",
-        "http://127.0.0.1:5173",
-        "http://192.168.1.6:8080",
+        "http://127.0.0.1:8081",
+        "http://192.168.1.4:8081",
     ],
     allow_credentials=True,
     allow_methods=["*"],
