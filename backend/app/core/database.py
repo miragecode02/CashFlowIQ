cf599ded594dbdd39a4b9f4036be4475f4d34664
Mirage verma <miragecode02@gmail.com>
2026-08-07 22:37:08 +0530

    deplopy try 3

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 from app.core.config import settings
+from app.core.config import settings
 
+print("DATABASE_URL =", settings.DATABASE_URL)
 engine = create_async_engine(
     settings.DATABASE_URL,
     echo=settings.DEBUG,
